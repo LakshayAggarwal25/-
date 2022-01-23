ad71ece650f5d63a4597604f31a63c1c2f26d1a7
LAKSHAY AGGARWAL <lakshayaggarwal2507@gmail.com>
2022-01-23 16:31:54 +0530

    add file system except view

--- Project 4/temp.js
+++ Project 4/temp.js
@@ -3,12 +3,18 @@ let folderTemplate = document.querySelector(".folder-template");
 let folderContainer = document.querySelector(".folders-container");
 let breadCrumb = document.querySelector(".bread-crumb");
 let rootPath = document.querySelector(".path");
+
+let fileAddBtn = document.querySelector("#add-file-button");
+let fileTemplate = document.querySelector(".text-files-template");
+
 let currentFolderId = -1;
-let folderId = -1;
-let folders = [];
+let resourceId = -1;
+let resources = [];
 
 folderAddBtn.addEventListener("click",addFolder);
 rootPath.addEventListener("click",navigateBreadCrumb);
+
+fileAddBtn.addEventListener("click",addFile);
 
 function addFolder(){
     let folderName = prompt("Enter folder name ");
@@ -17,31 +23,62 @@ function addFolder(){
             alert("Folder name can't be more than 15 characters");
             return;
         }
-        let folderNameExists = folders.filter(f => f.pid == currentFolderId).some(folder => folder.name == folderName);
+        let folderNameExists = resources.filter(f => f.pid == currentFolderId).filter(r => r.resourceType == "folder").some(folder => folder.name == folderName);
         if(!folderNameExists){
-            folderId++;
-            folders.push(
+            resourceId++;
+            resources.push(
                 {
-                    id : folderId,
+                    id : resourceId,
                     name : folderName,
-                    pid : currentFolderId
+                    pid : currentFolderId,
+                    resourceType : "folder"
                 }
             );
-            addFolderToHTML(folderName,folderId,currentFolderId);
+            addFolderToHTML(folderName,resourceId,currentFolderId);
             saveToLocalStorage();
         }else{
             alert("Folder name already exists, please enter new name");
         }
     }
 }
+function addFile(){
+    let fileName = prompt("Enter file name ");
+    if(fileName){
+       if(fileName.length>15){
+           alert("File name can't be more than 15 characters");
+           return;
+       } 
+       let fileNameExists = resources.filter(r=>r.pid==currentFolderId).filter(r => r.resourceType == "file").some(r=>r.name == fileName);
+       if(!fileNameExists){
+           resourceId++;
+            resources.push({
+                id : resourceId,
+                name : fileName,
+                pid :currentFolderId,
+                resourceType : "file"
+            });
+
+            addFileToHTML(fileName,resourceId,currentFolderId);
+
+            saveToLocalStorage();
+       }else{
+           alert("File name already exists !!!");
+       }
+    }
+}
+
 
 function navigateBreadCrumb(){
     // let fname = this.innerHTML;
-    let currentFolderId = parseInt(this.getAttribute("fid"));
+    currentFolderId = parseInt(this.getAttribute("fid"));
 
     folderContainer.innerHTML = "";
-    folders.filter(f=>f.pid == currentFolderId).forEach(f=>{
-        addFolderToHTML(f.name,f.id,f.pid);
+    resources.filter(f=>f.pid == currentFolderId).forEach(resource=>{
+        if(resource.resourceType=="folder"){
+            addFolderToHTML(resource.name,resource.id,resource.pid);
+        }if(resource.resourceType=="file"){
+            addFileToHTML(resource.name,resource.id,resource.pid);
+        }
     })
 
     while(this.nextSibling){
@@ -67,8 +104,12 @@ function viewFolder(){
     breadCrumb.appendChild(pathContainer);
 
     folderContainer.innerHTML = "";
-    folders.filter(f=>f.pid == currentFolderId).forEach(f=>{
-        addFolderToHTML(f.name,f.id,f.pid);
+    resources.filter(f=>f.pid == currentFolderId).forEach(resource=>{
+        if(resource.resourceType=="folder"){
+            addFolderToHTML(resource.name,resource.id,resource.pid);
+        }if(resource.resourceType=="file"){
+            addFileToHTML(resource.name,resource.id,resource.pid);
+        }
     })
 }
 
@@ -89,8 +130,12 @@ function viewFolderByImg(){
     breadCrumb.appendChild(pathContainer);
 
     folderContainer.innerHTML = "";
-    folders.filter(f=>f.pid == currentFolderId).forEach(f=>{
-        addFolderToHTML(f.name,f.id,f.pid);
+    resources.filter(f=>f.pid == currentFolderId).forEach(resource=>{
+        if(resource.resourceType=="folder"){
+            addFolderToHTML(resource.name,resource.id,resource.pid);
+        }if(resource.resourceType=="file"){
+            addFileToHTML(resource.name,resource.id,resource.pid);
+        }
     })
 }
 
@@ -105,10 +150,10 @@ function editFolder(){
             return;
         }
         if(newFolderName!=oldFolderName){
-            let folderNameAlreadyExists = folders.filter(folder=>folder.pid == currentFolderId).some(folder=>folder.name == newFolderName);
+            let folderNameAlreadyExists = resources.filter(folder=>folder.pid == currentFolderId).some(folder=>folder.name == newFolderName);
             if(!folderNameAlreadyExists){   
                 // Updating in folders array
-                let folderInArray = folders.filter(folder=>folder.pid == currentFolderId).find(f => f.name == oldFolderName);
+                let folderInArray = resources.filter(folder=>folder.pid == currentFolderId).find(f => f.name == oldFolderName);
                 folderInArray.name = newFolderName;
                 //Updating on html
                 oldFolder.innerText = newFolderName;
@@ -122,6 +167,36 @@ function editFolder(){
         }
     }
 }
+function editFile(){
+    let fileBox = this.parentNode.parentNode.parentNode.parentNode;
+    let file = fileBox.querySelector(".text-file-name");
+    let fileName = file.innerText;
+    let newFileName = prompt("Enter new name for " + fileName);
+    let fileId = parseInt(file.getAttribute("fid"));
+    if(newFileName){
+        if(newFileName.length > 15){
+            alert("File name can't be more than 15 characters");
+            return;
+        }
+        if(newFileName!=fileName){
+
+            let fileNameAlreadyExists = resources.filter(folder=>folder.pid == currentFolderId).filter(r => r.resourceType == "file").some(folder=>folder.name == newFileName);
+            if(!fileNameAlreadyExists){   
+                // Updating in folders array
+                let fileInArray = resources.filter(r => r.id == fileId);
+                fileInArray.name = newFileName;
+                //Updating on html
+                file.innerText = newFileName;
+                // updating in local storage
+                saveToLocalStorage();
+            }else{
+                alert("File name already Exists!!!");
+            }
+        }else{
+            alert("This is same as old File name, enter something new");
+        }
+    }
+}
 
 function deleteFolder(){
     let folder = this.parentNode.parentNode.parentNode.parentNode;
@@ -132,16 +207,31 @@ function deleteFolder(){
 
     if(res){
         
-        let containsFolders = folders.some(f => f.pid == folderIdToBeDelete);
+        let containsFolders = resources.some(f => f.pid == folderIdToBeDelete);
         if(!containsFolders){
 
-            let folderIndexInArray = folders.findIndex(f => f.id == folderIdToBeDelete);
-            folders.splice(folderIndexInArray,1);
+            let folderIndexInArray = resources.findIndex(f => f.id == folderIdToBeDelete);
+            resources.splice(folderIndexInArray,1);
             folderContainer.removeChild(folder);
             saveToLocalStorage();
         }else{
             alert("Can't delete, folder contains other folders")
         }
+    }
+}
+function deleteFile(){
+    let fileBox = this.parentNode.parentNode.parentNode.parentNode;
+    let file = fileBox.querySelector(".text-file-name");
+    let fileName = file.innerText; 
+    let res = confirm("Are you sure you want to delete " + fileName);
+    let fileIdToBeDelete = parseInt(file.getAttribute("fid"));
+
+    if(res){
+
+            let folderIndexInArray = resources.findIndex(f => f.id == fileIdToBeDelete);
+            resources.splice(folderIndexInArray,1);
+            folderContainer.removeChild(fileBox);
+            saveToLocalStorage();
     }
 }
 
@@ -184,26 +274,74 @@ function addFolderToHTML(folderName,folderID,parentID){
     folderContainer.appendChild(folder);
 
 }
+function addFileToHTML(fileName,fileId,parentId){
+    let textFile = fileTemplate.content.querySelector(".text-file-box");
+    let tFile  = document.importNode(textFile,true);
 
-function saveToLocalStorage(){
-    let foldersString = JSON.stringify(folders);
-    localStorage.setItem("folders",foldersString);
+    let menuBtn = tFile.querySelector(".three-dots");
+    let menu = tFile.querySelector(".mini-menu");
+    let viewBtn = tFile.querySelector("[action='view']");
+    let editBtn = tFile.querySelector("[action='edit']");
+    let deleteBtn = tFile.querySelector("[action='delete']");
+    let fname = tFile.querySelector(".text-file-name");
+    let tFileImg = tFile.querySelector(".file-img");
+
+    fname.innerText = fileName;
+    fname.setAttribute("fid",fileId);
+    fname.setAttribute("pid",parentId);
+
+    let menuClosed = true;
+    function toggleMenuDisplay(){
+        if(menuClosed){
+            menu.style.display = "flex";
+        }else{
+            menu.style.display = "none";
+        }
+        menuClosed = !menuClosed;
+    }
+    menuBtn.addEventListener("click",toggleMenuDisplay);
+    menuBtn.parentNode.addEventListener("mouseleave",function(){
+        menu.style.display = "none";
+        menuClosed = true;
+    })
+
+    viewBtn.addEventListener("click",viewFile);
+    tFileImg.addEventListener("click",viewFileByImg);    
+    editBtn.addEventListener("click",editFile);
+    deleteBtn.addEventListener("click",deleteFile);
+
+    folderContainer.appendChild(tFile);
 }
 
+function saveToLocalStorage(){
+    let resourcesString = JSON.stringify(resources);
+    localStorage.setItem("resources",resourcesString);
+}
 function loadFromLocalStorage(){
-    let foldersString = localStorage.getItem("folders");
-    if(foldersString){
-        folders = JSON.parse(foldersString);
-        folders.forEach(folder => 
+    let resourcesString = localStorage.getItem("resources");
+    if(resourcesString){
+        resources = JSON.parse(resourcesString);
+        resources.forEach(resource => 
             {
-                if(folder.id > folderId){
-                    folderId = folder.id;
+                if(resource.id > resourceId){
+                    resourceId = resource.id;
                 }
-                if(folder.pid == currentFolderId){
-                    addFolderToHTML(folder.name,folder.id);
+                if(resource.pid == currentFolderId){
+                    if(resource.resourceType=="folder"){
+                        addFolderToHTML(resource.name,resource.id,resource.pid);
+                    }if(resource.resourceType=="file"){
+                        addFileToHTML(resource.name,resource.id,resource.pid);
+                    }
                 }
             }
         );
     }
 }
 loadFromLocalStorage();
+
+function viewFile(){
+
+}
+function viewFileByImg(){
+
+}
