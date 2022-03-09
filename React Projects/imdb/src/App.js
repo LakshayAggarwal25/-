2e7ec10ef90a89352b4d878c5bf770b42591f61c
LAKSHAY AGGARWAL <lakshayaggarwal2507@gmail.com>
2022-03-09 22:31:36 +0530

    movies project

--- React Projects/imdb/src/App.js
+++ React Projects/imdb/src/App.js
@@ -2,13 +2,25 @@ import './App.css';
 import Navbar from './components/navbar';
 import Banner from './components/banner';
 import Trending from './components/trending';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <Navbar></Navbar>
-      <Banner></Banner>
-      <Trending></Trending>
-    </>
+      <Routes>
+        <Route path="/" element={
+          <>
+            <Banner />
+            <Trending />
+          </>
+        } />
+        <Route path="movies" element={
+          <>
+            <Trending />
+          </>
+        } />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- React Projects/imdb/src/components/navbar.js
+++ React Projects/imdb/src/components/navbar.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import MovieLogo from "./assets/movie.jfif";
+import {Link} from 'react-router-dom';
 
 function navbar() {
     return (
         <>
             <div className="flex gap-8 items-center p-4 font-bold text-blue-900 text-lg md:text-xl lg:text-2xl md:p-8">
                 <img src={MovieLogo} className="h-16 cursor-pointer"></img>
-                <div className="cursor-pointer">Home</div>
-                <div className="cursor-pointer">Movies</div>
+                <Link to="/" className="cursor-pointer">Home</Link>
+                <Link to="movies" className="cursor-pointer">Movies</Link>
                 <div className="cursor-pointer">Favourites</div>
             </div>
         </>
